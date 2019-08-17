9f0d736794cfcab6b59ac83cd6f44a954b6c5b86
Moaney <ladonnamoney@gmail.com>
2019-08-16 20:45:04 -0400

    created tab component. not getting individual items from array at this time, however

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,16 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then((res) => {
+        res.data.topics.map((item => {
+            let divTags = document.createElement("div");
+            divTags.classList.add("tab")
+            document.querySelector(".topics").appendChild(divTags);
+        }))
+    })
+    .catch(err => console.log(err));
+
